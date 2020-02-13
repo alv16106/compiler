5cd640d85fb7d67c3a3c8236b096a8ea0c9ce783
Rodrigo Alvarado <alv16106@uvg.edu.gt>
2020-02-13 12:33:57 -0600

    add sumitas homework

---
+++ sumitas.py
@@ -0,0 +1,47 @@
+symbols = '*/+-'
+
+def analize(s):
+    ops = []
+    numbers = []
+    last = ''
+    for c in s:
+        if c in symbols:
+            ops.append(c)
+        else:
+            if last.isdigit():
+                numbers[-1] = numbers[-1] + c
+            else:
+                numbers.append(c)
+        last = c
+    numbers = [int(n) for n in numbers]
+    return ops, numbers
+
+def operate(n1, n2, op):
+    if op == '*':
+        return n1 * n2
+    elif op == '/':
+        return n1 / n2
+    if op == '+':
+        return n1 + n2
+    return n1 - n2
+
+
+def evaluate(s):
+    while '(' in s:
+        sub = s[s.find('(')+1:s.find(')')]
+        temp_value = evaluate(sub)
+        s = s.replace('('+ sub + ')', str(temp_value))
+    ops, values = analize(s)
+    for symbol in symbols:
+        while symbol in ops:
+            oi = ops.index(symbol)
+            result = operate(values[oi], values[oi+1], symbol)
+            del values[oi:oi+2]
+            del ops[oi]
+            values.insert(oi, result)
+    return values[0]
+
+
+if __name__ == "__main__":
+    string = input('Ingrese su cadena: ')
+    print(evaluate(string))
