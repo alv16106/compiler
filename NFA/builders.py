b25badf60a570e93f06688aac5b89adb2e9fb214
Rodrigo Alvarado <alv16106@uvg.edu.gt>
2020-02-24 18:30:32 -0600

    Add kleene, star and question nfa builders

--- NFA/builders.py
+++ NFA/builders.py
@@ -2,6 +2,8 @@ from constants import *
 from state import State
 from nfa import NFA
 
+
+# single symbol accepting nfa
 def char(symbol):
     s0 = State()
     s1 = State(accepting=True)
@@ -9,34 +11,49 @@ def char(symbol):
     return NFA(s0, s1)
 
 
+# epsilon accepting nfa
 def epsilon():
     return char(EPSILON)
 
 
+# def concatenation between 2 nfas. nfa.nfa
 def concat(n1, n2):
     n1.end.accepting = False
     n1.end.addTransition(EPSILON, n2.start)
     return NFA(n1.start, n2.end)
 
 
+# def nfa | nfa
 def selection(n1, n2):
     new_in = State()
     new_out = State()
 
     new_in.addTransition(EPSILON, n1.start)
     new_in.addTransition(EPSILON, n2.start)
-    n1.out.addTransition(EPSILON, new_out)
-    n2.out.addTransition(EPSILON, new_out)
+    n1.end.addTransition(EPSILON, new_out)
+    n2.end.addTransition(EPSILON, new_out)
 
-    n2.out.accepting = False
-    n1.out.accepting = False
+    n2.end.accepting = False
+    n1.end.accepting = False
     new_out.accepting = True
 
     return NFA(new_in, new_out)
 
 
+# def Kleene nfa*
 def star(n1):
-    pass
+    n1.start.addTransition(EPSILON, n1.out)
+    n1.end.addTransition(EPSILON, n1.start)
+    return n1
 
+
+# def + nfa+
 def plus(n1):
-    pass
+    n1.end.addTransition(EPSILON, n1.start)
+    return n1
+
+
+# def symbol? == symbol | EPSILON
+def question(n1):
+    n1.start.addTransition(EPSILON, n1.end)
+    return n1
